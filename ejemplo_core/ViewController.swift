b00ecf4b1a0f703eb8145778560b89fb5350c5a0
HanselRamoscorp <hansel.ramoscorp@outlook.com>
2021-10-26 17:12:27 -0500

    Initial Commit

---
+++ ejemplo_core/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejemplo_core
+//
+//  Created by Hansel on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ejemplo_coreUITests/ejemplo_coreUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ejemplo_coreUITestsLaunchTests.swift
+//  ejemplo_coreUITests
+//
+//  Created by Hansel on 26/10/21.
+//
+
+import XCTest
+
+class ejemplo_coreUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
